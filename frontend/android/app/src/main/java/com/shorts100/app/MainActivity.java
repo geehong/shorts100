4a5b1c5b45353972d891cff06e51d8a4f805f84d
geehong <geecgpia@gmail.com>
2026-05-28 14:28:48 +0900

    fix: override WebView user agent to bypass Google GSI restriction

--- frontend/android/app/src/main/java/com/shorts100/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/shorts100/app/MainActivity.java
@@ -1,5 +1,17 @@
 package com.shorts100.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Google GSI WebView 차단 우회: Chrome 브라우저로 위장
+        WebSettings settings = this.bridge.getWebView().getSettings();
+        String originalUA = settings.getUserAgentString();
+        String chromeUA = originalUA.replace("; wv", "").replaceAll("Version/[\\d.]+", "");
+        settings.setUserAgentString(chromeUA);
+    }
+}
