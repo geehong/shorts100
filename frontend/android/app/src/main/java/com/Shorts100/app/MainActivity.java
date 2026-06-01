b982c009e2539df30e0e2fd77a985bb938612224
geehong <geecgpia@gmail.com>
2026-06-01 10:10:01 +0900

    fix: change onDestroy access modifier to public in MainActivity

--- frontend/android/app/src/main/java/com/Shorts100/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/Shorts100/app/MainActivity.java
@@ -154,7 +154,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         super.onDestroy();
         if (onDownloadCompleteReceiver != null) {
             try {
